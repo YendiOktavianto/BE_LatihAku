bd01de1b82196f1d4925f4eb0ab01e2dd581cea2
LAPTOP-5PJ49EUI\user <gerhanaabiwow@gmail.com>
2022-07-09 17:36:22 +0700

    myCoach and routes

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,7 +1,54 @@
 const express = require("express");
 const routes = express.Router();
 const userController = require("../controllers/userController");
+const coachController = require("../controllers/coachController");
+const placeController = require("../controllers/placeController");
+const categoryController = require("../controllers/categoryController");
+const bookingController = require("../controllers/bookingController");
+const myCoachController = require("../controllers/myCoachController");
 
-routes.post("/register", userController.register);
+//user
+routes.post("/register", userController.registerUser);
 routes.post("/login", userController.login);
+routes.put("/update/:id", userController.register);
+routes.delete("/delete/:id", userController.login);
+routes.get("/find/:id", userController.register);
+routes.get("/findAll", userController.login);
+
+//coach
+routes.post("/register", coachController.registerCoach);
+routes.post("/login", coachController.login);
+routes.put("/update/:id", coachController.register);
+routes.delete("/delete/:id", coachController.login);
+routes.get("/find/:id", coachController.register);
+routes.get("/findAll", coachController.login);
+
+//place
+routes.post("/create", placeController.register);
+routes.put("/update/:id", placeController.login);
+routes.delete("/delete/:id", placeController.register);
+routes.get("/find/:id", placeController.login);
+routes.get("/findAll", placeController.register);
+
+//booking
+routes.post("/create", bookingController.login);
+routes.put("/update/:id", bookingController.register);
+routes.delete("/delete/:id", bookingController.login);
+routes.get("/find/:id", bookingController.login);
+routes.get("/findAll", bookingController.register);
+
+//category
+routes.post("/create", categoryController.login);
+routes.put("/update/:id", categoryController.register);
+routes.delete("/delete/:id", categoryController.login);
+routes.get("/find/:id", categoryController.login);
+routes.get("/findAll", categoryController.register);
+
+//myCoach
+routes.post("/create", myCoachController.login);
+routes.put("/update/:id", myCoachController.register);
+routes.delete("/delete/:id", myCoachController.login);
+routes.get("/find/:id", myCoachController.login);
+routes.get("/findAll", myCoachController.register);
+
 module.exports = routes;
