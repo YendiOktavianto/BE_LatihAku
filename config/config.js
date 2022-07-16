82982e43374901fb435d037490ca121fb52e2114
LAPTOP-5PJ49EUI\user <gerhanaabiwow@gmail.com>
2022-07-16 21:36:21 +0700

    config clear

--- config/config.js
+++ config/config.js
@@ -1,21 +1,9 @@
 require("dotenv").config({ path: ".env" });
 const { DB_HOST, DB_USER, DB_NAME, DB_PASSWORD, DB_DIALECT } = process.env;
 
-let host = process.env.DB_HOST;
-let user = process.env.DB_USER;
-let name = process.env.DB_NAME;
-let password = process.env.DB_PASSWORD;
-let dialect = process.env.DB_DIALECT;
-
-console.log(host);
-console.log(user);
-console.log(name);
-console.log(password);
-console.log(dialect);
-
 module.exports = {
   development: {
-    username: DB_HOST,
+    username: DB_USER,
     password: DB_PASSWORD,
     database: DB_NAME,
     host: DB_HOST,
