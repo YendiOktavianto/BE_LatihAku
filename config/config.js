eb681397aee559a259aaed650b38debba4510bf3
Yendi Oktavianto <yendioktavianto99@gmail.com>
2022-07-16 21:30:38 +0700

    Edit cofig

--- config/config.js
+++ config/config.js
@@ -1,30 +1,38 @@
-require('dotenv').config()
-const{DB_HOST, 
-  DB_USERNAME, 
-  DB_NAME, 
-  DB_PASSWORD,
-  DB_DIALECT} = process.env
-  
-module.exports={
-  "development": {
-    "username": DB_USERNAME,
-    "password": DB_PASSWORD,
-    "database": DB_NAME,
-    "host": DB_HOST,
-    "dialect": DB_DIALECT
+require("dotenv").config({ path: ".env" });
+const { DB_HOST, DB_USER, DB_NAME, DB_PASSWORD, DB_DIALECT } = process.env;
+
+let host = process.env.DB_HOST;
+let user = process.env.DB_USER;
+let name = process.env.DB_NAME;
+let password = process.env.DB_PASSWORD;
+let dialect = process.env.DB_DIALECT;
+
+console.log(host);
+console.log(user);
+console.log(name);
+console.log(password);
+console.log(dialect);
+
+module.exports = {
+  development: {
+    username: DB_HOST,
+    password: DB_PASSWORD,
+    database: DB_NAME,
+    host: DB_HOST,
+    dialect: DB_DIALECT,
   },
-  "test": {
-    "username": "root",
-    "password": null,
-    "database": "database_test",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
+  test: {
+    username: "root",
+    password: null,
+    database: "database_test",
+    host: "127.0.0.1",
+    dialect: "mysql",
   },
-  "production": {
-    "username": "root",
-    "password": null,
-    "database": "database_production",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
-  }
-}
+  production: {
+    username: "root",
+    password: null,
+    database: "database_production",
+    host: "127.0.0.1",
+    dialect: "mysql",
+  },
+};
