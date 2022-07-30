f377a3e62d91279878939ed91194713d30781af1
LAPTOP-5PJ49EUI\user <gerhanaabiwow@gmail.com>
2022-07-30 18:18:18 +0700

    jwebson

--- helper/jwt.js
+++ helper/jwt.js
@@ -25,15 +25,14 @@ const verifyToken = (req, res, next) => {
     req.body.token ||
     req.query.token ||
     req.headers["x-access-token"] ||
-    req.headers.authorization ||
-    req.headers.authorization.split(" ")[1];
+    (req.headers.authorization && req.headers.authorization.split(" ")[1]);
 
   if (!token) {
     return res.status(403).send("A token is required for authentication");
   }
 
   try {
-    const decoded = jwt.verify(token, config.SECRET_KEY);
+    const decoded = jwt.verify(token, SECRET_KEY);
     req.user = decoded;
   } catch (err) {
     return res.status(401).send("Invalid Token");
